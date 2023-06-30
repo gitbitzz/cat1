d1f4e4652cf3a8d4c7aae71070684b1fed34f3a2
gitbitzz <138145405+gitbitzz@users.noreply.github.com>
2023-06-30 13:46:42 -0700

    Add files via upload

---
+++ cat1/QUE 2iii.java
@@ -0,0 +1,8 @@
+public class Employee {
+    public static void main(String[] args) {
+        Employee accountant = Employee("Raila O");
+        Employee programmer = Employee("George Airtel");
+        System.out.println(accountant);
+        System.out.println(programmer);
+    }
+}
